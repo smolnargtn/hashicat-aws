d1276b338f3d0f9fafc108ac4a50c5f87ce529bf
Stefan Molnar <stefan.molnar@infor.com>
2023-01-10 21:48:32 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -123,12 +123,12 @@ resource "aws_instance" "hashicat" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
-  Department                  = devops
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = devops
   }
 }
 
